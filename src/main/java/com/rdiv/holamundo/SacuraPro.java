b033ca11cac648b5b8229950326d34f16f1b7175
RobertDavidIpialesVasquez <davidipiales12345678910@gmail.com>
2023-11-08 09:24:35 -0500

    Initial commit

---
+++ src/main/java/com/rdiv/holamundo/SacuraPro.java
@@ -0,0 +1,14 @@
+package com.rdiv.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SacuraPro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sacura_pro);
+    }
+}
